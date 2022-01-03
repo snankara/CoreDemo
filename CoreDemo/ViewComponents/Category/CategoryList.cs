d12a0b4d98f3a6e121178d5c85cd467d48568def
Sinan Kara <snan_kara_@hotmail.com>
2022-01-03 16:25:40 +0300

    Commit 16:25 3.01

---
+++ CoreDemo/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryList : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryList()
+        {
+            _categoryService = new CategoryManager(new EfCategoryDal());
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryService.GetAll();
+            return View(categories);
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentListByBlog()
+        {
+            _commentService = new CommentManager(new EfCommentDal());
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var comments = _commentService.GetAllByBlogId(id);
+            return View(comments);
+        }
+    }
+}
